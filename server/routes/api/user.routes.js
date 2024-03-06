c48323bbf309cbfa7daf4662b37a0312d57de4d3
KDeLaria <149177749+KDeLaria@users.noreply.github.com>
2024-03-06 16:28:23 -0600

    finished routes for user routes

--- server/routes/api/user.routes.js
+++ server/routes/api/user.routes.js
@@ -5,12 +5,14 @@ require("dotenv").config();
 const { getAllUsers, createUser, deleteUser, getUser,
     updateUser, loginHandler } = require("../../controllers/user.controller");
 
-    
+function setToken () {
+    return jwt.sign({ id: id }, process.env.JWT_SECRET);
+};
 
 router.get("/", async (req, res) => {
     try {
-        const data = await getAllUsers();
-        res.status(200).json(data);
+        const payload = await getAllUsers();
+        res.status(200).json(payload);
     } catch (err) {
         console.log(err.message);
         res.status(500).json({ status: "error", message: err.message });
@@ -19,22 +21,64 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
     try {
-        const data = await getUser(req.params.id);
-        res.status(200).json(data);
+        const payload = await getUser(req.params.id);
+        res.status(200).json(payload);
     } catch (err) {
         console.log(err.message);
         res.status(500).json({ status: "error", message: err.message });
     }
 });
 
-router.get("/:id", async (req, res) => {
+router.put("/:id", async (req, res) => {
     try {
-        const data = await getUser(req.params.id);
-        res.status(200).json(data);
+        const payload = await updateUser(req.params.id, req.body);
+        res.status(200).json(payload);
     } catch (err) {
         console.log(err.message);
         res.status(500).json({ status: "error", message: err.message });
     }
 });
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const payload = await deleteUser(req.params.id);
+        res.status(200).json(payload);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json({ status: "error", message: err.message });
+    }
+});
+
+router.post("/", async (req, res) => {
+    try {
+        const payload = await createUser(req.body);
+        const token = setToken(payload.id);
+        res.status(200).cookie("auth_cookie", token).json(payload);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json({ status: "error", message: err.message });
+    }
+});
+
+
+router.post("/logout", async(req, res) => {
+    try {
+      res.status(200).cookie("auth_cookie", "").json({ status: "success" })
+    }catch(err){
+      console.log(err.message);
+      res.status(401).json({ status: "error", message: "Could not you logout." })
+    }
+  });
+
+  router.post("/login", async(req, res) => {
+    try {
+      const payload = await loginHandler(req.body.username, req.body.password);
+      const token = createToken(payload._id);
+      res.status(200).cookie("auth_cookie", token).json(payload);
+    }catch(err){
+      console.log(err.message);
+      res.status(401).json({ status: "error", message: "Invalid username or password." })
+    }
+  });
 
 module.exports = router;
