a6eb31fca65dac05f792cac7d9b46fcb470d4151
Dan Brandt <dan.p.brandt@gmail.com>
2024-03-11 11:40:07 -0500

    nav header working

--- client/src/components/home/header.jsx
+++ client/src/components/home/header.jsx
@@ -23,21 +23,25 @@ const Header = () => {
   };
 
   return (
-    <header className="flex justify-between items-center mb-2 p-4 bg-gray-100 rounded-lg">
-      <h1 className="text-5xl font-extrabold" onClick={navigateHome} style={{cursor: 'pointer'}}>WOLF.IO</h1>
-      <nav>
-        <ul className="flex space-x-4">
-          <li><Link to="/search" className="py-3 px-6 bg-blue-600 text-white rounded-lg hover:bg-blue-800 transition-colors duration-300">Search</Link></li>
-          <li><Link to="/portfolio" className="py-3 px-6 bg-blue-600 text-white rounded-lg hover:bg-blue-800 transition-colors duration-300">Portfolio</Link></li>
-        </ul>
-      </nav>
-      {loggedIn && (<span>Welcome {name},</span>)}
-      <button 
-        className="py-3 px-6 bg-blue-600 text-white rounded-lg hover:bg-blue-800 transition-colors duration-300"
-        onClick={handleLoginClick}
-      >
-        {loggedIn ? 'Logout' : 'Login'}
-      </button>
+    <header className="flex flex-wrap justify-between items-center mb-2 p-2 md:p-4 bg-gray-100 rounded-lg">
+      <div className="flex items-center flex-wrap">
+        <h1 className="text-3xl md:text-5xl font-extrabold mr-2 md:mr-4" onClick={navigateHome} style={{cursor: 'pointer'}}>WOLF.IO</h1>
+        <nav>
+          <ul className="flex space-x-2 md:space-x-4">
+            <li><Link to="/search" className="py-2 px-3 md:py-3 md:px-6 text-sm md:text-base bg-blue-600 text-white rounded-lg hover:bg-blue-800 transition-colors duration-300">Search</Link></li>
+            <li><Link to="/portfolio" className="py-2 px-3 md:py-3 md:px-6 text-sm md:text-base bg-blue-600 text-white rounded-lg hover:bg-blue-800 transition-colors duration-300">Portfolio</Link></li>
+          </ul>
+        </nav>
+      </div>
+      <div className="mt-2 md:mt-0">
+        {loggedIn && (<span className="mr-2 md:mr-4">Welcome {name},</span>)}
+        <button 
+          className="py-2 px-3 md:py-3 md:px-6 text-sm md:text-base bg-blue-600 text-white rounded-lg hover:bg-blue-800 transition-colors duration-300"
+          onClick={handleLoginClick}
+        >
+          {loggedIn ? 'Logout' : 'Login'}
+        </button>
+      </div>
     </header>
   );
 };
