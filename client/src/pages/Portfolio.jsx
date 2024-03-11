2ea541ddf867e627c1db908269dcef5d01cac777
thejackbitt <jack.bittner@outlook.com>
2024-03-10 22:51:40 -0500

    finishing touches to portfolio.jsx

--- client/src/pages/Portfolio.jsx
+++ client/src/pages/Portfolio.jsx
@@ -3,7 +3,7 @@ import Trendline from "../components/home/Trendline";
 import TrendlineHelper from "../components/portfolio/TrendlineHelper";
 
 function Portfolio() {
-  const portfolioArr = ["TSLA", "AAPL", "F"];
+  const portfolioArr = ["TSLA", "AAPL", "F", "AMD", "MSFT", "WBD", "HPE", "T", "GOOG", "CMCSA", "PYPL"];
   const [trendlineProps, setTrendlineProps] = useState({ portfolioArr });
 
   const handleButtonClick = (symbol) => {
@@ -16,9 +16,13 @@ function Portfolio() {
 
   return (
     <>
-      <div>
-        <Trendline {...trendlineProps} />
-        <TrendlineHelper onButtonClick={handleButtonClick} />
+      <div className="flex">
+        <div className="flex-initial w-2/3">
+          <Trendline {...trendlineProps} />
+        </div>
+        <div className="flex-initial w-1/3">
+          <TrendlineHelper portfolioArr={portfolioArr} onButtonClick={handleButtonClick} />
+        </div>
       </div>
     </>
   );
