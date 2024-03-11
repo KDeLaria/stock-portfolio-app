27d4c9fe2dd5eb0eeb5a23cea1ea2b4df854a2e8
Dan Brandt <dan.p.brandt@gmail.com>
2024-03-11 11:27:25 -0500

    Adding nav links to header

--- client/src/components/home/header.jsx
+++ client/src/components/home/header.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import {useAuth} from "../../utils/Auth";
 
 const Header = () => {
   const navigate = useNavigate(); // Initialize useNavigate
   const {loggedIn, logout, name}  = useAuth();
+
+  
   // Function to handle login button click
   const handleLoginClick = () => {
     if (loggedIn) {
@@ -14,20 +16,28 @@ const Header = () => {
     navigate('/login'); // Navigate to the login page
   }
   };
+
+  // Function to navigate back home when WOLF.IO is clicked
+  const navigateHome = () => {
+    navigate('/'); // Navigate to the home page
+  };
+
   return (
     <header className="flex justify-between items-center mb-2 p-4 bg-gray-100 rounded-lg">
-      <h1 className="text-5xl font-extrabold">WOLF.IO</h1>
+      <h1 className="text-5xl font-extrabold" onClick={navigateHome} style={{cursor: 'pointer'}}>WOLF.IO</h1>
       <nav>
-        {/* Add navigation links if needed */}
+        <ul className="flex space-x-4">
+          <li><Link to="/search" className="py-3 px-6 bg-blue-600 text-white rounded-lg hover:bg-blue-800 transition-colors duration-300">Search</Link></li>
+          <li><Link to="/portfolio" className="py-3 px-6 bg-blue-600 text-white rounded-lg hover:bg-blue-800 transition-colors duration-300">Portfolio</Link></li>
+        </ul>
       </nav>
       {loggedIn && (<span>Welcome {name},</span>)}
       <button 
         className="py-3 px-6 bg-blue-600 text-white rounded-lg hover:bg-blue-800 transition-colors duration-300"
-        onClick={handleLoginClick} // Add the click handler here
-      >    
-        {loggedIn ? (<span>Logout</span>) : (<span>Login</span>)}
+        onClick={handleLoginClick}
+      >
+        {loggedIn ? 'Logout' : 'Login'}
       </button>
-     
     </header>
   );
 };
