9c8e3e0e6fa855b19a9bd295b98a17c52c1c9380
Dan Brandt <dan.p.brandt@gmail.com>
2024-03-12 14:08:03 -0500

    Seeding script for News Data

--- updateNewsData.js
+++ updateNewsData.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const axios = require('axios');
-const apiUrl = 'https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=demo';
+const apiUrl = 'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&apikey=demo';
 
 
 axios.get(apiUrl)
   .then(response => {
     const data = JSON.stringify(response.data, null, 2);
-    fs.writeFile('./client/src/seeds/top-gainers-losers-seed.json', data, (err) => {
+    fs.writeFile('./client/src/seeds/news-seed.json', data, (err) => {
         if (err) throw err;
-        console.log('Data has been updated!');
+        console.log('News Data has been updated!');
       });;
   })
   .catch(error => {
