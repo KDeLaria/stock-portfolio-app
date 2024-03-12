fdd8b856406ca54f7210225dbf2f26bf6c8fbcbc
Dan Brandt <dan.p.brandt@gmail.com>
2024-03-12 15:06:55 -0500

    news and trending stocks now dont use api calls at all

--- client/src/components/home/trending.jsx
+++ client/src/components/home/trending.jsx
@@ -1,41 +1,12 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import fallbackData from "../../seeds/top-gainers-losers-seed.json";
 
 const Trending = ({ onButtonClick }) => {
-  const [topGainers, setTopGainers] = useState([]);
-  const [topLosers, setTopLosers] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const apiKey = "process.env.API_KEY"
-      const url = `https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=${apiKey}`;
-  
-      try {
-        const response = await axios.get(url);
-        console.log(response.data); // Inspect the actual structure
-  
-        // Hypothetical adjustment based on the actual API response structure
-        if (response.data && response.data.result && response.data.result.top_gainers && response.data.result.top_losers) {
-          setTopGainers(response.data.result.top_gainers.slice(0, 2));
-          setTopLosers(response.data.result.top_losers.slice(0, 2));
-        } else {
-          throw new Error("Invalid API response structure");
-        }
-      } catch (error) {
-        console.error("Error fetching stock data or invalid structure:", error);
-        // Fallback to local data
-        setTopGainers(fallbackData.top_gainers.slice(0, 2));
-        setTopLosers(fallbackData.top_losers.slice(0, 2));
-      }
-    };
-  
-    fetchData();
-  }, []);
+  // use data from the JSON file
+  const topGainers = fallbackData.top_gainers.slice(0, 2);
+  const topLosers = fallbackData.top_losers.slice(0, 2); 
 
   const handleClick = (e) => {
-    // Ensure the click handler logic correctly targets the element you expect
-    // This might need adjustment based on your actual markup and event target
     const clickText = e.target.closest('div').querySelector('span').innerText;
     console.log(clickText);
     onButtonClick(clickText);
@@ -55,7 +26,6 @@ const Trending = ({ onButtonClick }) => {
           >
             <span className="font-bold text-lg">{stock.ticker}</span>
             <div className="flex items-center text-green-600">
-              {/* SVG and change_percentage display */}
               {stock.change_percentage}
             </div>
           </div>
@@ -68,7 +38,6 @@ const Trending = ({ onButtonClick }) => {
           >
             <span className="font-bold text-lg">{stock.ticker}</span>
             <div className="flex items-center text-red-600">
-              {/* SVG and change_percentage display */}
               {stock.change_percentage}
             </div>
           </div>
